0bc67f00d4dcc5c4b5177c2fe76cb74fe5713c24
Деружинский Дмитрий <91655905+Terebellum@users.noreply.github.com>
2022-01-17 21:41:46 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,26 @@
+def calculator(number, program):
+    calculus = number
+    for i in program:
+        if i == "1":
+            calculus = calculus+2
+        if i == "2":
+            calculus = calculus*3
+    return calculus
+def findprogram(start, end):
+    m = "12*"
+    for i in m:
+        for j in m:
+            for l in m:
+                for s in m:
+                    for r in m:
+                        program = "".join([i, j, l, s, r])
+                        if calculator(start, program) == end:
+                            return program
+    return None
+                
+
+
+
+
+if __name__ == '__main__':
+    print(findprogram(0, 22))
